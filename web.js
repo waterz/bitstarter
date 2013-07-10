e519a8bf50a9565b742ac9604e89e7b43bc313e3
waterz <johnjtwaters@yahoo.co.uk>
2013-07-10 13:52:42 +0000

    att5

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
 
   
-  response.send(buf.toString(fs.readFile('index.html')));
+  response.send(buf.toString(fs.readFileSync('index.html')));
 });
 
 var port = process.env.PORT || 5000;
