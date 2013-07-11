2df63c1ea8de65a430e43dc0924060b1692f08d0
waterz <johnjtwaters@yahoo.co.uk>
2013-07-11 00:39:30 +0000

    same

--- grader.js
+++ grader.js
@@ -31,7 +31,8 @@ var URL_DEFAULT = "www.google.com";
 
 
 var checkURL = function(inURL) {
-
+    console.log("Not functional yet.");
+    return;
 };
 
 var assertFileExists = function(infile) {
@@ -72,7 +73,7 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .option('-u, --url <url_add>', 'address of page', clone(assertFileExists), URL_DEFAULT)
+        .option('-u, --url <url_add>', 'address of page', clone(checkURL), URL_DEFAULT)
 	.parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
