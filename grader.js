20de4a19b33be27bc24c2c72fb2b320401583dfa
waterz <johnjtwaters@yahoo.co.uk>
2013-07-11 00:36:43 +0000

    update grader

--- grader.js
+++ grader.js
@@ -24,8 +24,15 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "www.google.com";
+
+
+var checkURL = function(inURL) {
+
+};
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -65,7 +72,8 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .parse(process.argv);
+        .option('-u, --url <url_add>', 'address of page', clone(assertFileExists), URL_DEFAULT)
+	.parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
